585b256510ed30fe43c3308635b16e9efeb9f084
KaiD3v <kaikricardo99@gmail.com>
2026-04-14 16:55:31 -0300

    feature: add a not-found page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { HomePage } from './Pages/HomePage'
 import { AcousticPage } from './Pages/AcousticPage'
 import { OzzbornsPage } from './Pages/OzzbornsPage'
 import { SideRockPage } from './Pages/SideRockPage'
+import { NotFound } from './Pages/NotFound'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/acoustic" element={<AcousticPage />} />
         <Route path="/ozzborns" element={<OzzbornsPage />} />
         <Route path="/side-rock" element={<SideRockPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
