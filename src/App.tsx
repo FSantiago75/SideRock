cbd5a8cb278a7b11023ef8eab4400e9fc1a5b650
KaiD3v <kaikricardo99@gmail.com>
2026-04-14 16:28:23 -0300

    fix: base directory from /SideRock to /

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import './App.css'
 
 function App() {
   return (
-    <BrowserRouter basename="/SideRock/">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/acoustic" element={<AcousticPage />} />
